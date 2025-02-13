8102f3843a670b65f08b954599dac8c15ee3be3f
Oussama Farah <oufarah@e1r12p4.1337.ma>
2025-02-13 08:56:08 +0100

    all good exept leaks

--- Mandatory/exec.c
+++ Mandatory/exec.c
@@ -6,59 +6,77 @@
 /*   By: oufarah <oufarah@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 01:10:29 by oufarah           #+#    #+#             */
-/*   Updated: 2025/02/13 03:52:49 by oufarah          ###   ########.fr       */
+/*   Updated: 2025/02/13 08:55:41 by oufarah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+int	check_exit_status(void)
+{
+	int	status;
+	int	check;
+
+	while (wait(&status) != -1)
+	{
+		check = status;
+	}
+	if (WIFSIGNALED(check) || WEXITSTATUS(check) != 0)
+		return (-1);
+	return (0);
+}
+
+void	parent_thing(int *fd, t_exec **head, int *i)
+{
+	dup2(fd[0], 0);
+	close(fd[0]);
+	close(fd[1]);
+	*head = (*head)->next;
+	(*i)++;
+}
+
+void	setup_child(int *fd, char *path, t_exec *head, t_fds *fds)
+{
+	if (!head->next)
+	{
+		close(fd[1]);
+		fd[1] = fds->out;
+	}
+	if (!fds->i)
+	{
+		dup2(fds->in, 0);
+		close(fds->in);
+	}
+	close(fd[0]);
+	dup2(fd[1], 1);
+	close(fd[1]);
+	head->cmd = get_cmd_path(head->cmd, path);
+}
+
 int	exec(t_exec *head, int ac, char **av, char *path)
 {
-	int	i;
-	int	fd_in;
-	int	fd_out;
-	int	fd[2];
-	int	pid;
+	t_fds	fds;
+	int		fd[2];
+	int		pid;
 
-	i = 0;
-	fd_in = open(av[1], O_RDONLY);
-	fd_out = open(av[ac - 1], O_CREAT | O_WRONLY, 0777);
+	(1) && (fds.i = 0, fds.in = open(av[1], O_RDONLY));
+	fds.out = open(av[ac - 1], O_CREAT | O_WRONLY, 0777);
 	while (head)
 	{
 		pipe(fd);
 		pid = fork();
 		if (pid == -1)
-			return (close(fd[0]), close(fd[1]), close(fd_in), close(fd_out), 0);
+			return (close(fd[0]), close(fd[1]),
+				close(fds.in), close(fds.out), 0);
 		if (pid == 0)
 		{
-			if (!head->next)
-			{
-				close(fd[1]);
-				fd[1] = fd_out;
-			}
-			if (!i)
-			{
-				dup2(fd_in, 0);
-				close(fd_in);
-			}
-			close(fd[0]);
-			dup2(fd[1], 1);
-			close(fd[1]);
-			head->cmd = get_cmd_path(head->cmd, path);
+			setup_child(fd, path, head, &fds);
 			execve(head->cmd, head->opt, NULL);
 			perror("pipex");
 			exit(errno);
 		}
 		else
-		{
-			dup2(fd[0], 0);
-			close(fd[0]);
-			close(fd[1]);
-			head = head->next;
-			i++;
-		}
+			parent_thing(fd, &head, &fds.i);
 	}
-	while (wait(NULL) != -1)
-		;
-	return (1);
+	return (check_exit_status());
 }
